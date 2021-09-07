d33c483eb529cc8dd25efa8dc11be65d3fdef39a
Lars van der Laan <larsvanderlaan@berkeley.edu>
2021-09-06 18:18:53 -0700

    front end functions added

---
+++ R/Lrnr_hal9001_semiparametric.R
@@ -0,0 +1,256 @@
+#' Scalable Highly Adaptive Lasso (HAL) semiparametric
+#'
+#' The Highly Adaptive Lasso (HAL) is a nonparametric regression function that
+#' has been demonstrated to optimally estimate functions with bounded (finite)
+#' variation norm. The algorithm proceeds by first building an adaptive basis
+#' (i.e., the HAL basis) based on indicator basis functions (or higher-order
+#' spline basis functions) representing covariates and interactions of the
+#' covariates up to a pre-specified degree. The fitting procedures included in
+#' this learner use \code{\link[hal9001]{fit_hal}} from the \pkg{hal9001}
+#' package. For details on HAL regression, consider consulting the following
+#' \insertCite{benkeser2016hal;textual}{sl3}),
+#' \insertCite{coyle2020hal9001-rpkg;textual}{sl3}),
+#' \insertCite{hejazi2020hal9001-joss;textual}{sl3}).
+#'
+#' @docType class
+#'
+#' @importFrom R6 R6Class
+#' @importFrom origami folds2foldvec
+#' @importFrom stats predict quasibinomial
+#'
+#' @export
+#'
+#' @keywords data
+#'
+#' @return A learner object inheriting from \code{\link{Lrnr_base}} with
+#'  methods for training and prediction. For a full list of learner
+#'  functionality, see the complete documentation of \code{\link{Lrnr_base}}.
+#'
+#' @format An \code{\link[R6]{R6Class}} object inheriting from
+#'  \code{\link{Lrnr_base}}.
+#'
+#' @family Learners
+#'
+#' @section Parameters:
+#'   - \code{...}: Arguments passed to \code{\link[hal9001]{fit_hal}}. See
+#'    it's documentation for details.
+#'
+#' @examples
+#' data(cpp_imputed)
+#' covs <- c("apgar1", "apgar5", "parity", "gagebrth", "mage", "meducyrs")
+#' task <- sl3_Task$new(cpp_imputed, covariates = covs, outcome = "haz")
+#'
+#' # instantiate with max 2-way interactions, 0-order splines, and binning
+#' # (i.e., num_knots) that decreases with increasing interaction degree
+#' hal_lrnr <- Lrnr_hal9001$new(
+#'   max_degree = 2, num_knots = c(20, 10), smoothness_orders = 0
+#' )
+#' hal_fit <- hal_lrnr$train(task)
+#' hal_preds <- hal_fit$predict()
+Lrnr_hal9001_semiparametric <- R6Class(
+  classname = "Lrnr_hal9001_semiparametric",
+  inherit = Lrnr_base, portable = TRUE, class = TRUE,
+  public = list(
+    initialize = function(formula_sp, family = NULL, interaction_variable = "A", ...) {
+      params <- sl3:::args_to_list()
+      super$initialize(params = params, ...)
+    }
+  ),
+  private = list(
+    .properties = c("continuous", "binomial", "weights", "ids"),
+    .train = function(task) {
+      args <- self$params
+      
+     
+      formula_sp <- args$formula_sp
+      trt <- args$interaction_variable
+      A <- task$data[[trt]]
+      W <- as.matrix(task$X)
+      W <- W[, setdiff(task$nodes$covariates, trt)]
+      V <- model.matrix(formula_sp, as.data.frame(W))
+      
+      outcome_type <- self$get_outcome_type(task)
+      args$X <- W
+      args$X_unpenalized <- as.matrix(A*V)
+      args$Y <- outcome_type$format(task$Y)
+      
+      if (is.null(args$family)) {
+        args$family <- outcome_type$glm_family()
+      }
+      
+      if (!any(grepl("fit_control", names(args)))) {
+        args$fit_control <- list()
+      }
+      args$fit_control$foldid <- origami::folds2foldvec(task$folds)
+      
+      if (task$has_node("id")) {
+        args$id <- task$id
+      }
+      
+      if (task$has_node("weights")) {
+        args$fit_control$weights <- task$weights
+      }
+      
+      if (task$has_node("offset")) {
+        args$offset <- task$offset
+      }
+      
+      # fit HAL, allowing glmnet-fitting arguments
+      other_valid <- c(
+        names(formals(glmnet::cv.glmnet)), names(formals(glmnet::glmnet))
+      )
+      
+      fit_object <- sl3:::call_with_args(
+        hal9001::fit_hal, args,
+        other_valid = other_valid
+      )
+      
+      return(fit_object)
+    },
+    
+    .predict = function(task = NULL) {
+      args <- self$params
+      formula_sp <- args$formula_sp
+      trt <- args$interaction_variable
+      A <- task$data[[trt]]
+      W <- as.matrix(task$X)
+      W <- W[, setdiff(task$nodes$covariates, trt)]
+      V <- model.matrix(formula_sp, as.data.frame(W))
+      X_unpenalized <- as.matrix(A*V)
+      predictions <- stats::predict(
+        self$fit_object,
+        new_data = as.matrix(W),
+        new_X_unpenalized = X_unpenalized
+      )
+      if (!is.na(safe_dim(predictions)[2])) {
+        p <- ncol(predictions)
+        colnames(predictions) <- sprintf("lambda_%0.3e", self$params$lambda)
+      }
+      return(predictions)
+    },
+    
+    .required_packages = c("hal9001", "glmnet")
+  )
+)
+
+
+
+
+#' Scalable Highly Adaptive Lasso (HAL)
+#'
+#' The Highly Adaptive Lasso (HAL) is a nonparametric regression function that
+#' has been demonstrated to optimally estimate functions with bounded (finite)
+#' variation norm. The algorithm proceeds by first building an adaptive basis
+#' (i.e., the HAL basis) based on indicator basis functions (or higher-order
+#' spline basis functions) representing covariates and interactions of the
+#' covariates up to a pre-specified degree. The fitting procedures included in
+#' this learner use \code{\link[hal9001]{fit_hal}} from the \pkg{hal9001}
+#' package. For details on HAL regression, consider consulting the following
+#' \insertCite{benkeser2016hal;textual}{sl3}),
+#' \insertCite{coyle2020hal9001-rpkg;textual}{sl3}),
+#' \insertCite{hejazi2020hal9001-joss;textual}{sl3}).
+#'
+#' @docType class
+#'
+#' @importFrom R6 R6Class
+#' @importFrom origami folds2foldvec
+#' @importFrom stats predict quasibinomial
+#'
+#' @export
+#'
+#' @keywords data
+#'
+#' @return A learner object inheriting from \code{\link{Lrnr_base}} with
+#'  methods for training and prediction. For a full list of learner
+#'  functionality, see the complete documentation of \code{\link{Lrnr_base}}.
+#'
+#' @format An \code{\link[R6]{R6Class}} object inheriting from
+#'  \code{\link{Lrnr_base}}.
+#'
+#' @family Learners
+#'
+#' @section Parameters:
+#'   - \code{...}: Arguments passed to \code{\link[hal9001]{fit_hal}}. See
+#'    it's documentation for details.
+#'
+#' @examples
+#' data(cpp_imputed)
+#' covs <- c("apgar1", "apgar5", "parity", "gagebrth", "mage", "meducyrs")
+#' task <- sl3_Task$new(cpp_imputed, covariates = covs, outcome = "haz")
+#'
+#' # instantiate with max 2-way interactions, 0-order splines, and binning
+#' # (i.e., num_knots) that decreases with increasing interaction degree
+#' hal_lrnr <- Lrnr_hal9001$new(
+#'   max_degree = 2, num_knots = c(20, 10), smoothness_orders = 0
+#' )
+#' hal_fit <- hal_lrnr$train(task)
+#' hal_preds <- hal_fit$predict()
+Lrnr_hal9001 <- R6Class(
+  classname = "Lrnr_hal9001",
+  inherit = Lrnr_base, portable = TRUE, class = TRUE,
+  public = list(
+    initialize = function(formula_hal= NULL, ...) {
+      params <- sl3:::args_to_list()
+      super$initialize(params = params, ...)
+    }
+  ),
+  private = list(
+    .properties = c("continuous", "binomial", "weights", "ids"),
+    .train = function(task) {
+      args <- self$params
+      args$formula <- args$formula_hal
+      args$X <- as.matrix(task$X)
+      
+      outcome_type <- self$get_outcome_type(task)
+      args$Y <- outcome_type$format(task$Y)
+      
+      if (is.null(args$family)) {
+        args$family <- outcome_type$glm_family()
+      }
+      
+      if (!any(grepl("fit_control", names(args)))) {
+        args$fit_control <- list()
+      }
+      args$fit_control$foldid <- origami::folds2foldvec(task$folds)
+      
+      if (task$has_node("id")) {
+        args$id <- task$id
+      }
+      
+      if (task$has_node("weights")) {
+        args$fit_control$weights <- task$weights
+      }
+      
+      if (task$has_node("offset")) {
+        args$offset <- task$offset
+      }
+      
+      # fit HAL, allowing glmnet-fitting arguments
+      other_valid <- c(
+        names(formals(glmnet::cv.glmnet)), names(formals(glmnet::glmnet))
+      )
+      
+      fit_object <- sl3:::call_with_args(
+        hal9001::fit_hal, args,
+        other_valid = other_valid
+      )
+      
+      return(fit_object)
+    },
+    
+    .predict = function(task = NULL) {
+      predictions <- stats::predict(
+        self$fit_object,
+        new_data = data.matrix(task$X)
+      )
+      if (!is.na(safe_dim(predictions)[2])) {
+        p <- ncol(predictions)
+        colnames(predictions) <- sprintf("lambda_%0.3e", self$params$lambda)
+      }
+      return(predictions)
+    },
+    
+    .required_packages = c("hal9001", "glmnet")
+  )
+)
+
