6a5c542d7f0b03df53ed80f0f23b4a24195d74b0
Lars van der Laan <larsvanderlaan@berkeley.edu>
2021-09-12 20:35:19 -0700

    new pdf

---
+++ R/refit_glm.R
@@ -0,0 +1,18 @@
+
+refit_glm <- function(fit_object, formula, estimand = fit_object$estimand, verbose  =TRUE) {
+  likelihood <- fit_object$tmle3_fit$likelihood$initial_likelihood
+  tmle3_input <- fit_object$tmle3_input
+  data <- tmle3_input$data
+  node_list <- tmle3_input$node_list
+  learner_list <- NULL
+  delta_epsilon <- tmle3_input$delta_epsilon
+  levels_A <- tmle3_input$levels_A
+  if(inherits(fit_object, "npglm") || inherits(fit_object, "msmglm")) {
+    tmle_spec <- tmle3_Spec_npCausalGLM$new(likelihood_override = likelihood, formula = formula, estimand = estimand, delta_epsilon = delta_epsilon, verbose = verbose, treatment_level = levels_A)
+  } else {
+    stop("Reusing fits is not supported for spglm")
+  }
+   tmle3_fit <- suppressMessages(suppressWarnings(tmle3(tmle_spec, data, node_list, learner_list)))
+  
+  return(tmle3_fit)
+}
